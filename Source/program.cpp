fb95ecf282deef7afbe4424d382a3cc5ff58285f
José Martínez <marquevjosman@gmail.com>
2024-09-26 23:23:02 -0600

    up

--- Source/program.cpp
+++ Source/program.cpp
@@ -1,17 +1,34 @@
 #include "muxa/scales.hpp"
 #include "muxa/synths/sya.hpp"
+#include "muxa/types.hpp"
 #include "muxa/wave.hpp"
+using namespace muxa;
 auto main() -> int {
-  muxa::Scale scale;
-  muxa::Wave wave(6);
-  muxa::Sya synth;
-  synth.frequencyM = scale.frequencyOf(0);
-  synth.wave(wave);
-  synth.timeM += 2;
-  synth.frequencyM = scale.frequencyOf(0 + 1);
-  synth.wave(wave);
-  synth.timeM += 2;
-  synth.frequencyM = scale.frequencyOf(0 + 1 + 1);
-  synth.wave(wave);
+  Scale scale(180, 5, 9);
+  Wave wave(20);
+  Sya synth;
+  synth.durationM = 1.0 / 4;
+  real adder = synth.durationM;
+  for (u64 i = 0; i < 12; i++) {
+    synth.frequencyM = scale.frequencyOf(0);
+    synth.wave(wave);
+    synth.timeM += adder;
+    synth.frequencyM = scale.frequencyOf(0 + 4);
+    synth.wave(wave);
+    synth.timeM += adder;
+    synth.frequencyM = scale.frequencyOf(0 + 4 + 3);
+    synth.wave(wave);
+    synth.timeM += adder;
+    synth.frequencyM = scale.frequencyOf(0);
+    synth.wave(wave);
+    synth.timeM += adder;
+    synth.frequencyM = scale.frequencyOf(0 + 3);
+    synth.wave(wave);
+    synth.timeM += adder;
+    synth.frequencyM = scale.frequencyOf(0 + 3 + 4);
+    synth.wave(wave);
+    synth.timeM += adder;
+  }
+
   wave.normalize(0.75).save();
 }
